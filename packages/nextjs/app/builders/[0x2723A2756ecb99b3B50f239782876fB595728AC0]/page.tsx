0e62abff3ab03cd3119bc038617e1fc781abb7a1
Arjun Sasikumar <itsarjn@gmail.com>
2024-04-09 15:49:32 +0530

    Added personal profile section

---
+++ packages/nextjs/app/builders/[0x2723A2756ecb99b3B50f239782876fB595728AC0]/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import type { NextPage } from "next";
+
+const PersonalPage: NextPage = () => {
+  return (
+    <div>
+      <iframe style={container} src="https://arjun.wtf"></iframe>
+    </div>
+  );
+};
+
+const container = {
+  height: "100vh",
+  width: "100vw",
+};
+
+export default PersonalPage;

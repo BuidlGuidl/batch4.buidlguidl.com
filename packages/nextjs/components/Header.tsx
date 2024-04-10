4c7c2cb0bb2dda29f0a4e12a19f91427c52bf6b2
Yash Goyal <yashgo0018@gmail.com>
2024-04-10 20:00:20 +0530

    Added different symbols in the header for the different wallet checked in status (#12)
    * added icons for allowlist and checked in status
    
    * fixed  a bug
    
    * fixed messages on the wallet info icons and added daisy ui tooltips

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,6 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import WalletBatchInfo from "./WalletBatchInfo";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
@@ -102,6 +103,7 @@ export const Header = () => {
         </ul>
       </div>
       <div className="navbar-end flex-grow mr-4">
+        <WalletBatchInfo />
         <RainbowKitCustomConnectButton />
         <FaucetButton />
       </div>

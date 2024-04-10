8b8d238ab0f23856851aa6dfa322a77d53ca99e4
Boris Kolev <bkolev95@gmail.com>
2024-04-10 20:02:14 +0300

    * Addressed PR review comments

--- packages/nextjs/app/builders/0xF11046328814004583c8e74050f0d59520f53e90/page.tsx
+++ packages/nextjs/app/builders/0xF11046328814004583c8e74050f0d59520f53e90/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import type { NextPage } from "next";
 
 interface ProfileLink {
   name: string;
@@ -7,37 +7,40 @@ interface ProfileLink {
 
 const profileLinks: ProfileLink[] = [
   { name: "GitHub", url: "https://github.com/Welith" },
-  { name: "Twitter", url: "https://twitter.com/bo4ka7a" }, // Replace with URL
+  { name: "Twitter", url: "https://twitter.com/bo4ka7a" },
 ];
 
-const Home: React.FC = () => {
+const Home: NextPage = () => {
   return (
-    <div style={{ textAlign: "center", paddingTop: "50px" }}>
+    <div className="text-center pt-20">
       <header>
-        <h1>Boris Kolev</h1>
-        <p>Passionate about technology</p>
+        <h1 className="text-4xl font-bold">Boris Kolev</h1>
+        <p className="text-lg">Passionate about technology</p>
       </header>
-      <section style={{ marginBottom: "2rem" }}>
-        <img
-          id="avatar"
-          src="https://avatars.githubusercontent.com/u/33628158?s=400&u=aa5a292688f39acc845bf95765266f1ce040fd35&v=4"
-          alt="Boris Kolev - Avatar"
-          style={{ borderRadius: "50%", width: "200px", height: "200px", margin: "0 auto 2rem" }}
-        />
+      <section className="mb-8">
+        <div className="rounded-full w-40 h-40 mx-auto mb-8 overflow-hidden">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://avatars.githubusercontent.com/u/33628158?s=400&u=aa5a292688f39acc845bf95765266f1ce040fd35&v=4"
+            alt="Boris Kolev - Avatar"
+            width={200}
+            height={200}
+          />
+        </div>
       </section>
-      <section style={{ marginBottom: "2rem" }}>
-        <h2>About Me</h2>
-        <p>
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold">About Me</h2>
+        <p className="text-lg">
           Backend web2 developer with around 5 years of experience. Web3 rookie, aiming to go into Smart Contract
           Security.
         </p>
       </section>
-      <section style={{ marginBottom: "2rem" }}>
-        <h2>Connect with me</h2>
-        <ul style={{ listStyle: "none", padding: 0 }}>
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold">Connect with me</h2>
+        <ul className="list-none p-0">
           {profileLinks.map(link => (
-            <li key={link.name} style={{ display: "inline-block", margin: "0 1rem" }}>
-              <a href={link.url} style={{ textDecoration: "underline", color: "#007bff" }}>
+            <li key={link.name} className="inline-block mr-4">
+              <a href={link.url} className="underline text-blue-500">
                 {link.name}
               </a>
             </li>

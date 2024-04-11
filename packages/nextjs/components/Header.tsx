736ec66c29bd53764d9471c9f6cad3b9e79a1596
Carlos Sánchez <oceanrdn@gmail.com>
2024-04-11 15:40:15 +0200

    Image alt

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -91,7 +91,7 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/batch-4-logo.png" />
+            <Image alt="Batch 4 logo" className="cursor-pointer" fill src="/batch-4-logo.png" />
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Batch #4</span>

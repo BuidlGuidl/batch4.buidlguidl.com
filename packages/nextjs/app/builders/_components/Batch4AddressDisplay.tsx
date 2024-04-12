950b74bf1cd7132c10f9a7b856a63030010d0315
Victor Forbes <vhforbes@gmail.com>
2024-04-12 15:32:06 -0300

    fix: updated to use semantic classes

--- packages/nextjs/app/builders/_components/Batch4AddressDisplay.tsx
+++ packages/nextjs/app/builders/_components/Batch4AddressDisplay.tsx
@@ -9,12 +9,12 @@ const Batch4AddressDisplay = ({ address, index }: { address: string; index: numb
   const isDarkMode = resolvedTheme === "dark";
 
   return (
-    <div className="flex flex-col items-center justify-center rounded-lg p-5 bg-white shadow-md">
+    <div className="flex flex-col items-center justify-center rounded-lg p-5 bg-base-content shadow-md">
       <div className="mx-auto h-32 w-32 rounded-full overflow-hidden shadow-lg mb-4 relative">
         <Batch4Avatar address={address} size={128} />
       </div>
-      <h3 className="text-lg font-semibold text-center text-black">Builder {index + 1}</h3>
-      <div className="text-gray-600 text-sm text-center mt-1">
+      <h3 className="text-lg font-semibold text-center text-primary">Builder {index + 1}</h3>
+      <div className="text-secondary text-sm text-center mt-1">
         <Batch4Address address={address} format="short" size="base" />
       </div>
       <div className="mt-4">
--- packages/nextjs/app/builders/page.tsx
+++ packages/nextjs/app/builders/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { NextPage } from "next";
-import { useTheme } from "next-themes";
 import Batch4AddressDisplay from "~~/app/builders/_components/Batch4AddressDisplay";
 import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
@@ -18,30 +17,18 @@ const BuildersPage: NextPage = () => {
       .filter((builder): builder is string => !!builder) || [];
   const uniqueBuilders = Array.from(new Set(builderAddresses));
 
-  const { resolvedTheme } = useTheme();
-  const isDarkMode = resolvedTheme === "dark";
-
   return (
-    <div
-      className={`flex flex-col items-center justify-center min-h-screen ${isDarkMode ? "bg-gray-750" : "bg-gray-200"}`}
-    >
-      <div className={`max-w-7xl mx-auto p-8 ${isDarkMode ? "bg-gray-800" : "bg-white"} shadow-2xl rounded-2xl my-10`}>
-        <h1 className={`text-4xl font-extrabold text-center ${isDarkMode ? "text-white" : "text-gray-900"} mb-10`}>
-          Our Builders
-        </h1>
+    <div className={`flex flex-col items-center justify-center min-h-screen bg-base-200`}>
+      <div className={`max-w-7xl mx-auto p-8 bg-base-300 shadow-2xl rounded-2xl my-10`}>
+        <h1 className={`text-4xl font-extrabold text-center text-neutral mb-10`}>Our Builders</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {isLoading ? (
             <div className="col-span-1 sm:col-span-2 lg:col-span-4 flex justify-center items-center">
-              <span className={`loading loading-spinner ${isDarkMode ? "text-white" : "text-info"}`}></span>
+              <span className={`loading loading-spinner text-info`}></span>
             </div>
           ) : (
             uniqueBuilders.map((builder, index) => (
-              <div
-                key={index}
-                className={`bg-white border-2 border-gray-200 shadow-sm rounded-lg overflow-hidden ${
-                  isDarkMode ? "dark:bg-gray-900" : ""
-                }`}
-              >
+              <div key={index} className={`bg-neutral border-2 border-gray-200 shadow-sm rounded-lg overflow-hidden `}>
                 <Batch4AddressDisplay address={builder} index={index} />
               </div>
             ))

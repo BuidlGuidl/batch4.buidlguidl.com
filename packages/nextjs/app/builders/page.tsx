268e23fad8004a787fce11ae48a68fe4f8826b12
Victor Forbes <vhforbes@gmail.com>
2024-04-14 17:21:31 -0300

    fix: changed clases to double quotes

--- packages/nextjs/app/builders/page.tsx
+++ packages/nextjs/app/builders/page.tsx
@@ -18,17 +18,17 @@ const BuildersPage: NextPage = () => {
   const uniqueBuilders = Array.from(new Set(builderAddresses));
 
   return (
-    <div className={`flex flex-col items-center justify-center min-h-screen bg-base-200`}>
-      <div className={`max-w-7xl mx-auto p-8 bg-base-300 shadow-2xl rounded-2xl my-10`}>
-        <h1 className={`text-4xl font-extrabold text-center text-neutral mb-10`}>Our Builders</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-base-200">
+      <div className="max-w-7xl mx-auto p-8 bg-base-300 shadow-2xl rounded-2xl my-10">
+        <h1 className="text-4xl font-extrabold text-center text-neutral mb-10">Our Builders</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {isLoading ? (
             <div className="col-span-1 sm:col-span-2 lg:col-span-4 flex justify-center items-center">
-              <span className={`loading loading-spinner text-info`}></span>
+              <span className="loading loading-spinner text-info"></span>
             </div>
           ) : (
             uniqueBuilders.map((builder, index) => (
-              <div key={index} className={`bg-neutral border-2 border-gray-200 shadow-sm rounded-lg overflow-hidden `}>
+              <div key={index} className="bg-neutral border-2 border-gray-200 shadow-sm rounded-lg overflow-hidden">
                 <Batch4AddressDisplay address={builder} index={index} />
               </div>
             ))

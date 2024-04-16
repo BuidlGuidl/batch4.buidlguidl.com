415a7bfcca07be25ff8a4a53ed1eeb585f76f13a
Gustavo Barbosa Sanchez <11802440@usp.br>
2024-04-16 09:01:45 -0300

    removes blockexplorer card

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, CodeBracketIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BugAntIcon, CodeBracketIcon } from "@heroicons/react/24/outline";
 import { CheckedInChart } from "~~/components/CheckedInChart";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
@@ -40,16 +40,6 @@ const Home: NextPage = () => {
                 Tinker with your smart contract using the{" "}
                 <Link href="/debug" passHref className="link">
                   Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
                 </Link>{" "}
                 tab.
               </p>

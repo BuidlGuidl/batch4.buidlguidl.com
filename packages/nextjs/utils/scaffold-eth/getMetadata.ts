002651c8eabcb22ee55117c1c8d0244f054a5c09
Bhavya Gor <bhavya.gor9999@gmail.com>
2024-04-16 23:48:04 +0530

    initial changes to metadata for rendering frames

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
 
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : `http://localhost:${process.env.PORT || 3000}`;
+const titleTemplate = "%s | Buidlguidl Batch #4";
+
 export const getMetadata = ({
   title,
   description,
@@ -9,16 +14,20 @@ export const getMetadata = ({
   description: string;
   imageRelativePath?: string;
 }): Metadata => {
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : `http://localhost:${process.env.PORT || 3000}`;
   const imageUrl = `${baseUrl}${imageRelativePath}`;
   return {
-    title: title,
+    metadataBase: new URL(baseUrl),
+    title: {
+      default: title,
+      template: titleTemplate,
+    },
     description: description,
     openGraph: {
-      title: title,
-      description: description,
+      title: {
+        default: title,
+        template: titleTemplate,
+      },
+      description,
       images: [
         {
           url: imageUrl,
@@ -26,9 +35,26 @@ export const getMetadata = ({
       ],
     },
     twitter: {
-      title: title,
-      description: description,
+      card: "summary_large_image",
       images: [imageUrl],
+      title: {
+        default: title,
+        template: titleTemplate,
+      },
+      description,
+    },
+    icons: {
+      icon: [{ url: imageUrl, sizes: "32x32", type: "image/png" }],
+    },
+    other: {
+      "fc:frame": "vNext",
+      "fc:frame:image": imageUrl,
+      "fc:frame:button:1": "Checkout the Buidlguidl Batch #4",
+      "fc:frame:button:1:action": "link",
+      "fc:frame:button:1:target": baseUrl,
+      "fc:frame:button:2": "Checkout buidlguidl.com",
+      "fc:frame:button:2:action": "link",
+      "fc:frame:button:2:target": "https://buidlguidl.com",
     },
   };
 };

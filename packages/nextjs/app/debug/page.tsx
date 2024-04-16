a7e403d19a166953798858890509721ebeddc76b
Victor Forbes <vhforbes@gmail.com>
2024-04-16 20:57:09 -0300

    feat: colors refresh

--- packages/nextjs/app/debug/page.tsx
+++ packages/nextjs/app/debug/page.tsx
@@ -11,7 +11,7 @@ const Debug: NextPage = () => {
   return (
     <>
       <DebugContracts />
-      <div className="text-center mt-8 bg-secondary p-10">
+      <div className="text-center mt-8 bg-primary p-10">
         <h1 className="text-4xl my-0">Debug Contracts</h1>
         <p className="text-neutral">
           You can debug & interact with your deployed contracts here.
